9b42531f040aeb9718d27d487edcdcc0109e4b40
Ahmed El Tayeb <52711045+AhmedEltayeb0@users.noreply.github.com>
2023-09-09 14:29:03 +0300

    #End Of  Booking API#

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php
 
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+//Start of web app 
